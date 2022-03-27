6278fc937229d17bfe6cd5c0a898272abb2eeb30
Stephen Lie Juliando Soetopo <SS37685X@cimbniaga.co.id>
2022-03-27 17:02:56 +0700

    Remove unused import

--- src/main/java/com/myproject/activitytrackerservice/web/ActivityTrackerController.java
+++ src/main/java/com/myproject/activitytrackerservice/web/ActivityTrackerController.java
@@ -2,19 +2,17 @@ package com.myproject.activitytrackerservice.web;
 
 import com.myproject.activitytrackerservice.domain.entity.UserActivity;
 import com.myproject.activitytrackerservice.service.UserActivityService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/api/activitytracker")
