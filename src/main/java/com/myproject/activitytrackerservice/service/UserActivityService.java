5888d6bd180a5d0601db4362586662a4c760e611
Stephen Lie Juliando Soetopo <SS37685X@cimbniaga.co.id>
2022-03-27 17:10:48 +0700

    remove unused import

--- src/main/java/com/myproject/activitytrackerservice/service/UserActivityService.java
+++ src/main/java/com/myproject/activitytrackerservice/service/UserActivityService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import javax.sql.rowset.serial.SerialClob;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 
 @Service
 @Slf4j
